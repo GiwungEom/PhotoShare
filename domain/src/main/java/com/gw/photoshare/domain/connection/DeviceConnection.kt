f8c9b0af25287c2d56543d6c397bd0acf0f1300c
Kiwung <kiwung0701@gmail.com>
2026-03-05 23:33:13 +0900

    Rename isAvailable to isReadyToScan in DeviceConnection

--- domain/src/main/java/com/gw/photoshare/domain/connection/DeviceConnection.kt
+++ domain/src/main/java/com/gw/photoshare/domain/connection/DeviceConnection.kt
@@ -3,7 +3,7 @@ package com.gw.photoshare.domain.connection
 import kotlinx.coroutines.flow.Flow
 
 interface DeviceConnection {
-    val isAvailable: Flow<Boolean>
+    val isReadyToScan: Flow<Boolean>
     suspend fun scan(): List<Device>
     suspend fun connect(device: Device)
     fun disconnect()
